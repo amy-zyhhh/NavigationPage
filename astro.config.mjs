24860da183368811c8362cd2a2e583b1f09c0133
zhou_N510 <2945099066@qq.com>
2026-07-16 21:50:34 +0800

    Add GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://amy-zyhhh.github.io',
+  base: '/NavigationPage',
+});
